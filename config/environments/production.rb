abe7161382fac8eb4da246c5e34714cf21a8cff3
Patrick O'Brien <patrick@patrickmobrien.com>
2020-04-10 17:21:54 -0400

    update mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {host: "http://chirpa.dev"}
+  config.action_mailer.default_url_options = {host: "https://chirpa.dev"}
+  config.active_job.queue_adapter = :sidekiq
+  config.action_mailer.perform_deliveries = true
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
